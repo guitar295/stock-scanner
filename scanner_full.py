3c12886f37529178c6254b0ff82d63661aa16726
Mr.Hoang <minhtruong295@gmail.com>
2026-04-02 13:42:00 +0700

    Update scanner_full.py

--- scanner_full.py
+++ scanner_full.py
@@ -55,7 +55,7 @@ TELEGRAM_BOT_TOKEN  = os.environ.get('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHAT_ID    = os.environ.get('TELEGRAM_CHAT_ID')
 MY_PERSONAL_CHAT_ID = os.environ.get('MY_PERSONAL_CHAT_ID')
 
-DATA_SOURCE        = 'VCI'
+DATA_SOURCE        = 'KBS'
 SCAN_INTERVAL_SEC  = 120
 TZ_VN              = pytz.timezone('Asia/Ho_Chi_Minh')
 
